a013d6232b59cf6cc0bec9b25adf936029941d25
Rana Emad <rana3emad@gmail.com>
2020-03-06 14:08:10 +0200

    add unique email constraint

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -9,46 +9,58 @@ class UsersController{
         this.setValidationRules();
     }
 
-    store(req,res){
-        const data={
-            "name":req.body.name,
-            "email":req.body.email,
-            "password":bcrypt.hashSync(req.body.password,saltRounds)
-        };
-        User.create(data)
-        .then((result)=>{
-            res.status(201).json({"id":result.insertId});
-        })
-        .catch((err)=>{
-            res.json({"msg":"service unavailable"});
-        });
+    async store(req,res){
+        let exists = await User.findUserByEmail(req.body.email);
+        if(exists!=false){
+            res.status(400).json({"errors":{"email":"The email already exists."}});
+        }
+        else{
+            const data={
+                "name":req.body.name,
+                "email":req.body.email,
+                "password":bcrypt.hashSync(req.body.password,saltRounds)
+            };
+            User.create(data)
+            .then((result)=>{
+                res.status(201).json({"id":result.insertId});
+            })
+            .catch((err)=>{
+                res.json({"msg":"service unavailable"});
+            });
+        }
     }
 
-    update(req,res){
-        User.findById(req.user.id)
-        .then((exists)=>{
-            if(exists==false){
-                res.status(400).json({"msg":"resource not found"});
-            }
-            else{
-                
-                const data= {
-                    "name":req.body.name,
-                    "email":req.body.email
-                };
-                User.update(req.params.id,data)
-                .then((result)=>{
-                    res.status(200).json({"id":req.params.id, "name":data.name,"email":data.email});
-                })
-                .catch((err)=>{
-                    res.json({"msg":"service unavailable"});
-                });
+    async update(req,res){
+        let exists = await User.findUserByEmail(req.body.email);
+        if(exists!=false && exists[0].id!=req.user.id){
+            res.status(400).json({"errors":{"email":"The email already exists."}});
+        }
+        else{
+            User.findById(req.user.id)
+            .then((exists)=>{
+                if(exists==false){
+                    res.status(400).json({"msg":"resource not found"});
+                }
+                else{
+                    
+                    const data= {
+                        "name":req.body.name,
+                        "email":req.body.email
+                    };
+                    User.update(req.user.id,data)
+                    .then((result)=>{
+                        res.status(200).json({"id":req.params.id, "name":data.name,"email":data.email});
+                    })
+                    .catch((err)=>{
+                        res.json({"msg":"service unavailable"});
+                    });
 
-            }
-        })
-        .catch((err)=>{
-            res.json({"msg":"service unavailable"});
-        });
+                }
+            })
+            .catch((err)=>{
+                res.json({"msg":"service unavailable"});
+            });
+        }
 
     }
 
